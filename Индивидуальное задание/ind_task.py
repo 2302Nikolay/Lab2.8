0b085a6dc3cf5eacea3c9b56da4bc8d45d08acdd
2302Nikolay <Malzewn@gmail.com>
2022-10-08 08:52:54 +0300

    Исправил индивидуальное задание ещё раз

--- Индивидуальное задание/ind_task.py
+++ Индивидуальное задание/ind_task.py
@@ -28,37 +28,39 @@ def add(list_man):
 
 
 def list_d(list_man):
-    # Заголовок таблицы.
-    line = '+-{}-+-{}-+-{}-+-{}-+'.format(
-        '-' * 4,
-        '-' * 30,
-        '-' * 20,
-        '-' * 20
-    )
-    print(line)
-    print(
-        '| {:^4} | {:^30} | {:^20} | {:^20} |'.format(
-            "No",
-            "Имя",
-            "Номер телефона",
-            "Дата рождения"
-        )
-    )
-    print(line)
+   if list_man:
+       # Заголовок таблицы.
+       line = '+-{}-+-{}-+-{}-+-{}-+'.format(
+           '-' * 4,
+           '-' * 30,
+           '-' * 20,
+           '-' * 20
+       )
+       print(line)
+       print(
+           '| {:^4} | {:^30} | {:^20} | {:^20} |'.format(
+               "No",
+               "Имя",
+               "Номер телефона",
+               "Дата рождения"
+           )
+       )
+       print(line)
 
-    # Вывести данные о человеке.
-    for idx, man in enumerate(list_man, 1):
-        print(
-            '| {:>4} | {:<30} | {:<20} | {:<20}  |'.format(
-                idx,
-                man.get('name', ''),
-                man.get('number', ''),
-                man.get('date', '')
-            )
-        )
+       # Вывести данные о человеке.
+       for idx, man in enumerate(list_man, 1):
+           print(
+               '| {:>4} | {:<30} | {:<20} | {:<20}  |'.format(
+                   idx,
+                   man.get('name', ''),
+                   man.get('number', ''),
+                   man.get('date', '')
+               )
+           )
 
-    print(line)
-
+       print(line)
+   else:
+       print("Список работников пуст: ")
 
 def select(command_d, mans_list):
     parts = command_d.split(' ', maxsplit=1)
